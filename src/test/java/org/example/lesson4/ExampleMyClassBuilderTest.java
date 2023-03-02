18aab8cedde33ccf661888ae50ba80de14e5b95f
Olga <chainikova@gmail.com>
2023-03-02 16:42:27 +0300

    4

---
+++ src/test/java/org/example/lesson4/ExampleMyClassBuilderTest.java
@@ -0,0 +1,30 @@
+package org.example.lesson4;
+
+
+import lesson44.MyClass;
+import lesson44.MyClassBuilder;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ExampleMyClassBuilderTest {
+
+    @Test
+    void createObject(){
+
+        MyClass myClass1 = new MyClass();
+        myClass1.setAttr1("");
+        myClass1.setAttr2(true);
+        myClass1.setAttr3(1l);
+
+        MyClass myClass2 = new MyClass("",true,1l);
+
+        MyClass myClass3 = new MyClassBuilder()
+                .setAttr1("")
+                .setAttr2(true)
+                .setAttr3(1l)
+                .createMyClass();
+
+        Assertions.assertEquals(myClass1,myClass3);
+        Assertions.assertFalse(myClass1==myClass3);
+    }
+}
---
+++ src/test/java/org/example/lesson4/ExampleTest.java
@@ -0,0 +1,45 @@
+package org.example.lesson4;
+
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
+
+public class ExampleTest extends AbstractTest{
+    @Test
+    void getRecipePositiveTest() {
+        given().spec(getRequestSpecification())
+                .when()
+                .get("https://api.spoonacular.com/recipes/716429/information")
+                .then()
+                .spec(responseSpecification);
+    }
+
+
+    @Test
+    void getAccountInfoWithExternalEndpointTest(){
+        Response response = given().spec(requestSpecification)
+                .when()
+                .formParam("title","Burger")
+                .post("https://api.spoonacular.com/recipes/cuisine").prettyPeek()
+                .then()
+                .extract()
+                .response()
+                .body()
+                .as(Response.class);
+        assertThat(response.getCuisine(), containsString("American"));
+    }
+
+    @Test
+    void test(){
+        given().spec(requestSpecification)
+                .when()
+                .formParam("title","Burger")
+                .formParam("language", "en")
+                .post("https://api.spoonacular.com/recipes/cuisine").prettyPeek()
+                .then()
+                .statusCode(200);
+    }
+}
